83542221dc0a461c6b061a498a577cda0cc6c5b4
FranciscoLicon <30940867+FranciscoLicon@users.noreply.github.com>
2021-03-03 11:25:19 -0600

    client validation

---
+++ SolListaGenerica/WebListaGenerica/Models/JS/Validacion.js
@@ -0,0 +1,23 @@
+﻿//Validación de formulario.
+const formulario = document.getElementById('form');
+
+const validar = (e) => {
+    const inputNombre = document.getElementById('Nombre'),
+        inputRaza = document.getElementById('Raza'),
+        inputEspecie = document.getElementById('Especie'),
+        inputSexo = document.getElementById('Sexo');
+        alertError = document.getElementById('alertError');
+
+    if (inputNombre.value == 0 || inputRaza.value == 0 || inputEspecie.value == 0 || inputSexo.value == 'sexo') {
+        e.preventDefault();
+        alertError.classList.remove('hide');
+        alertError.classList.add('show');
+        setTimeout(() => {
+            alertError.classList.remove('show');
+            alertError.classList.add('hide');
+        }, 2000);
+    }
+}
+
+//Eventos del formulario.
+formulario.addEventListener('submit', validar);
